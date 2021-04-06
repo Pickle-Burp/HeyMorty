f7696e773f03c416da438159a117e50e5f04bb89
Vinetos <Vinetosdev@gmail.com>
2021-04-06 21:51:20 +0200

    feat: Add sprec.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "sprec/sprec.h"
 
 int main() {
   printf("Hello, World!\n");
+  sprec_recognize_sync("fr", "fr", 100);
   return 0;
 }
---
+++ src/sprec/sprec.h
@@ -0,0 +1,13 @@
+//
+// Created by Vinetos on 06/04/2021.
+//
+
+#ifndef HEYMORTY_SRC_SPREC_SPREC_H_
+#define HEYMORTY_SRC_SPREC_SPREC_H_
+
+#include "wav.h"
+#include "flac_encoder.h"
+#include "web_client.h"
+#include "recognize.h"
+
+#endif //HEYMORTY_SRC_SPREC_SPREC_H_
