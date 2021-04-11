f4fc7b0fec960b3677994881109d002d757d23df
Vinetos <Vinetosdev@gmail.com>
2021-04-11 19:03:50 +0200

    fix: One main to run all project step

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
+#include <err.h>
+#include "neural_network/nn.h"
 #include "sprec/sprec.h"
 
 #define API_KEY      "API_KEY"
 #define LANG         "fr-FR"
-#define FILE_NAME    "src/assets/untitled.wav"
 
-int main() {
-  char *txt = sprec_recognize_wav(getenv(API_KEY), LANG,
-                                  FILE_NAME, 16000);
-  printf("%s", txt);
+int main(int argc, char **argv) {
+  if (argc < 2)
+    errx(EXIT_FAILURE, "No engough args. ./hey_morty <audio/nn>");
+  if (strcmp(argv[1], "audio") == 0) {
+    if (argc < 3)
+
+      errx(EXIT_FAILURE, "No engough args. ./hey_morty audio myFile.wav");
+    char *txt = sprec_recognize_wav(getenv(API_KEY), LANG,
+                                    argv[2], 16000);
+    printf("%s", txt);
+    return 0;
+  }
+  if (strcmp(argv[1], "nn") == 0) {
+    test_neural_network();
+    return 0;
+  }
   return 0;
 }
