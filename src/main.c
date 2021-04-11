3cd640b7b49657a4aac8add95aa53a2a2da006f6
Vinetos <Vinetosdev@gmail.com>
2021-04-11 12:35:22 +0200

    feat: Read api key from env variable

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include "sprec/sprec.h"
 
-#define API_KEY "AIzaSyA4e_DmX-kxzXvGiUtrkrRSTxM4Pa5zIEA"
-#define LANG    "fr-FR"
+#define API_KEY      "API_KEY"
+#define LANG         "fr-FR"
 #define FILE_NAME    "src/assets/untitled.wav"
 
 int main() {
-//  char *txt = sprec_recognize_base64(API_KEY, LANG,
-//                                   FILE_NAME, 16000);
-  char *txt = sprec_recognize_wav(API_KEY, LANG,
-                                   FILE_NAME, 16000);
+  char *txt = sprec_recognize_wav(getenv(API_KEY), LANG,
+                                  FILE_NAME, 16000);
   printf("%s", txt);
   return 0;
 }
