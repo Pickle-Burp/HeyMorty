2d00fd9b74dac89a47765e5ac359e6e06e6e6633
Lise Giraud <lise.giraud@epita.fr>
2021-05-01 18:51:02 +0200

    feat: Adding tensorflow

---
+++ src/neural_network/cnn.c
@@ -0,0 +1,16 @@
+//
+// Created by lise on 5/1/21.
+//
+
+#include "cnn.h"
+#include <tensorflow/c/c_api.h>
+#include <search.h>
+
+void test() {
+    TF_NewServer(0, 2, 0);
+}
+
+/*int main()
+{
+
+}*/
---
+++ src/neural_network/cnn.h
@@ -0,0 +1,8 @@
+//
+// Created by lise on 5/1/21.
+//
+
+#ifndef HEYMORTY_CNN_H
+#define HEYMORTY_CNN_H
+
+#endif //HEYMORTY_CNN_H
