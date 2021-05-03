25e04dae7f10d2ecfabbbac65c604e773aa6c4d1
Annabelle Chevreau <annabelle.chevreau@gmail.com>
2021-05-03 16:05:51 +0200

    execution ok pour toutes les ouvertures d'application

--- src/cmd/execution.c
+++ src/cmd/execution.c
@@ -3,49 +3,24 @@
 #include <err.h>
 #include <string.h>
 
-#define OPEN "open"
-#define SHOW "show"
-#define SEARCH "search"
+#define FALSE 0
+#define TRUE 1
 
-char **cut_text(char **text, int nb_word) {
-  char **cutting_command;
-  if (text[0] == "lance" || text[0] == "ouvre") {
-    cutting_command[0] = OPEN;
-    cutting_command[1] = text[1];
+int is_in(char *text, char *search, int nb_word){
+  int i = 0, found = FALSE;
+  while(i <= nb_word && found == FALSE){
+    if(text[i] == *search)
+      found = TRUE;
+    i++;
   }
-  if (text[0] == "montre-moi") {
-    cutting_command[0] = SHOW;
-    cutting_command[1] = text[nb_word - 2];
-    cutting_command[2] = text[nb_word - 1];
-  }
-  if (text[0] == "cherche") {
-    cutting_command[0] = SEARCH;
-    cutting_command[1] = text[nb_word - 1];
-    for (int i = 1; i < nb_word - 2; i++)
-      strcat(cutting_command[2], text[i]);
-  }
-  return cutting_command;
+  return found;
 }
 
-const char *convert_to_command(char **tab_command) {
+
+const char *convert_to_command(char **text, int nb_word) {
   char *command = "";
-  if (tab_command[0] == OPEN)
-    command = tab_command[1];
-  else if (tab_command[0] == SEARCH) {
-    strcat(command, tab_command[1]);
-    strcat(command, " ");
-    strcat(command, "\"");
-    strcat(command, tab_command[2]);
-    strcat(command, "\"");
-  } else if (tab_command[0] == SHOW) {
-    if (tab_command[1] == "dossier") {
-      strcat(command, "ls ");
-      strcat(command, tab_command[2]);
-    } else {
-      strcat(command, "cat");
-      strcat(command, tab_command[2]);
-    }
-  }
+  if(is_in(*text, "ouvre", nb_word) == TRUE)
+    command = text[nb_word - 1];
   return command;
 }
 
