e6267d3fdd8c84d428165888b46ee214269c524f
Annabelle Chevreau <annabelle.chevreau@gmail.com>
2021-06-15 13:03:38 +0200

    change hash function

--- src/cmd/spell2.c
+++ src/cmd/spell2.c
@@ -1,326 +1,91 @@
-#include "spell2.h"
+#include "spell_check.h"
 
-#define SIZE_DICT 235886
-#define ALPHABET_SIZE        (sizeof(alphabet) - 1)
-
-char *dictionary = "words.txt";
-
-const char alphabet[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+char *WORDS = "words.txt";
+// maximum length for a word
+const int MAX_WORD_LEN = 45;
+const int NB_LINES = 323579;
 
 
+int levenshtein(const char *s, int ls, const char *t, int lt){
+    int a, b, c;
 
+    /* if either string is empty, difference is inserting all chars
+     * from the other
+     */
+    if (!ls) return lt;
+    if (!lt) return ls;
 
-char *strtolower(char *word)
-{
-    for (char *s = word; *s; ++s) *s = tolower(*s);
-    return word;
+    /* if last letters are the same, the difference is whatever is
+     * required to edit the rest of the strings
+     */
+    if (s[ls - 1] == t[lt - 1])
+        return levenshtein(s, ls - 1, t, lt - 1);
+
+    /* else try:
+     *      changing last letter of s to that of t; or
+     *      remove last letter of s; or
+     *      remove last letter of t,
+     * any of which is 1 edit plus editing the rest of the strings
+     */
+    a = levenshtein(s, ls - 1, t, lt - 1);
+    b = levenshtein(s, ls, t, lt - 1);
+    c = levenshtein(s, ls - 1, t, lt);
+
+    if (a > b) a = b;
+    if (a > c) a = c;
+
+    return a + 1;
 }
 
-ENTRY *find(char *word)
-{
-    return hsearch((ENTRY){.key = word}, FIND);
-}
-
-int update(char *word)
-{
-    ENTRY *e = find(word);
-
-    if (!e) return 0;
-
-    e->data++;
-    return 1;
-}
-
-int readFile(const char* fileName, ENTRY dict)
-{
-    int fd = open(fileName, O_RDONLY);
-    if (fd < 0) return 0;
-
-    struct stat sb;
-    if (stat(dictionary, &sb)) return 0;
-    char *result = strdup(mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
-    if (result != MAP_FAILED)
-    {
-        char *w = NULL;
-        char *delimiter = "\n";
-        char *word = strtok(result, delimiter);
-        while(word)
-        {
-            w = strtolower(strdup(word));
-
-            if (!update(w))
-            {
-                dict.key  = w;
-                dict.data = (void *) 1;
-                hsearch(dict, ENTER);
-            }
-            word = strtok(NULL, delimiter);
-        }
-
-        close(fd);
-
-        return 1;
-    }
-    return -1;
-}
-
-char *substr(char *str, size_t offset, size_t limit)
-{
-    char *new_str;
-    size_t str_size = strlen(str);
-
-    if ((limit > str_size) || ((offset + limit) > str_size) || (str_size < 1) || (limit == 0)) return NULL;
-
-    new_str = malloc(limit+1);
-    if (!new_str) return NULL;
-
-    strncpy(new_str, str+offset, limit);
-    *(new_str + limit) = '\0';
-
-    return new_str;
-}
-
-static void *checked_malloc(int len)
-{
-    void *ret = malloc(len);
-
-    if (ret == NULL) {
-        fprintf(stderr, "Out of memory\n");
-        exit(0);
-    }
-    return ret;
-}
-
-/**
- * Takes a part of the source string and appends it to the destination string.
- *
- * @param     dst       Destination string to append to.
- * @param     dstLen    Current length of the destination string.  This will
- *                      be updated with the new length after appending.
- * @param     src       Source string.
- * @param     srcBegin  Starting index in the source string to copy from.
- * @param     len       Length of portion to copy.
- */
-static void append(char *dst, int *dstLen, const char *src, int srcBegin,
-        int len)
-{
-    if (len > 0) {
-        memcpy(&dst[*dstLen], &src[srcBegin], len);
-        *dstLen += len;
-    }
-    dst[*dstLen] = 0;
-}
-
-int deletion(char *word, char **array, int start_idx)
-{
-    int i = 0;
-    size_t word_len = strlen(word);
-
-    for (; i < word_len; i++)
-    {
-        int pos = 0;
-        array[i+start_idx] = checked_malloc(word_len);
-        append(array[i+start_idx], &pos, word, 0, i);
-        append(array[i+start_idx], &pos, word, i+1, word_len-(i+1));
-    }
-    return i;
-}
-
-int transposition(char *word, char **array, int start_idx)
-{
-    int i = 0;
-    size_t word_len = strlen(word);
-
-    for (; i < word_len-1; i++)
-    {
-        int pos = 0;
-        array[i+start_idx] = checked_malloc(word_len+1);
-        append(array[i+start_idx], &pos, word, 0,   i);
-        append(array[i+start_idx], &pos, word, i+1, 1);
-        append(array[i+start_idx], &pos, word, i,   1);
-        append(array[i+start_idx], &pos, word, i+2, word_len-(i+2));
-    }
-    return i;
-}
-
-int alteration(char *word, char **array, int start_idx)
-{
-    int k = 0;
-    size_t word_len = strlen(word);
-    char c[2] = {};
-
-    for (int i = 0; i < word_len; ++i)
-    {
-        for (int j = 0; j < ALPHABET_SIZE; ++j, ++k)
-        {
-            int pos = 0;
-            c[0] = alphabet[j];
-            array[k+start_idx] = checked_malloc(word_len+1);
-            append(array[k+start_idx], &pos, word, 0, i);
-            append(array[k+start_idx], &pos, c   , 0, 1);
-            append(array[k+start_idx], &pos, word, i+1, word_len-(i+1));
+char *correct(char *word, char **word_list){
+    int dist = levenshtein(word, strlen(word), word_list[0], strlen(word_list[0]));
+    int new_dist = dist;
+    char *res = word_list[0];
+    for(int i = 1; i < NB_LINES; i++){
+        new_dist = levenshtein(word, strlen(word), word_list[i], strlen(word_list[i]));
+        if(new_dist == 0)
+            return word;
+        if(new_dist < dist){
+            dist = new_dist;
+            res = word_list[i];
         }
     }
-    return k;
-}
-
-int insertion(char *word, char **array, int start_idx)
-{
-    int k = 0;
-    size_t word_len = strlen(word);
-    char c[2] = {};
-
-    for (int i = 0; i <= word_len; ++i)
-    {
-        for (int j = 0; j < ALPHABET_SIZE; ++j, ++k)
-        {
-            int pos = 0;
-            c[0] = alphabet[j];
-            array[k+start_idx] = checked_malloc(word_len+2);
-            append(array[k+start_idx], &pos, word, 0, i);
-            append(array[k+start_idx], &pos, c   , 0, 1);
-            append(array[k+start_idx], &pos, word, i, word_len-i);
-        }
-    }
-    return k;
-}
-
-size_t edits1_rows(char *word)
-{
-    size_t size = strlen(word);
-
-    return (size)                + // deletion
-    (size - 1)                   + // transposition
-    (size * ALPHABET_SIZE)       + // alteration
-    (size + 1) * ALPHABET_SIZE;    // insertion
-}
-
-char **edits1(char *word)
-{
-    int next_idx;
-    char **array = malloc(edits1_rows(word) * sizeof(char *));
-
-    if (!array) return NULL;
-
-    next_idx  = deletion(word, array, 0);
-    next_idx += transposition(word, array, next_idx);
-    next_idx += alteration(word, array, next_idx);
-    insertion(word, array, next_idx);
-
-    return array;
-}
-
-int array_exist(char **array, int rows, char *word)
-{
-    for (int i = 0; i < rows; ++i)
-    {
-        if (!strcmp(array[i], word)) return 1;
-    }
-    return 0;
-}
-
-char **known_edits2(char **array, int rows, int *e2_rows)
-{
-    size_t e1_rows = 0;
-    int res_size = 0;
-    int res_max  = 0;
-    char **res = NULL;
-    char **e1  = NULL;
-
-    for (int i = 0; i < rows; i++)
-    {
-        e1      = edits1(array[i]);
-        e1_rows = edits1_rows(array[i]);
-
-        for (int j = 0; j < e1_rows; j++)
-        {
-            if (find(e1[j]) && !array_exist(res, res_size, e1[j]))
-            {
-                if (res_size >= res_max) {
-                    // First time, allocate 50 entries.  After that, double
-                    // the size of the array.
-                    if (res_max == 0)
-                        res_max = 50;
-                    else
-                        res_max *= 2;
-                }
-                res = realloc(res, sizeof(char *) * res_max);
-                res[res_size++] = e1[j];
-            }
-        }
-    }
-
-    *e2_rows = res_size;
-
     return res;
 }
 
-char *max(char **array, int rows)
-{
-    char *max_word = NULL;
-    int max_size = 0;
-    ENTRY *e;
-
-    for (int i = 0; i < rows; i++)
-    {
-        e = find(array[i]);
-        if (e && ((int) e->data > max_size))
-        {
-            max_size = (int) e->data;
-            max_word = e->key;
-        }
+char** spell_check(char **text, int nb_word){
+    // load dictionary;
+    char *dict = WORDS;
+    char **word_list = calloc(NB_LINES + 1, sizeof(char));
+    //printf("non");
+    load(dict, word_list);
+    //printf("oui");
+    // spell-check each word
+    for(int i = 0; i < nb_word; i++){
+        printf("%s\n", text[i]);
+        text[i] = correct(text[i], word_list);
+        printf("%s\n", text[i]);
     }
-
-    return max_word;
+    return text;
 }
 
-void array_cleanup(char **array, int rows)
-{
-    for (int i = 0; i < rows; i++)
-    {
-        free(array[i]);
+/* Loads dict into memory. */
+void load(char *dict, char **word_list){
+    char word [MAX_WORD_LEN +1];
+    long i = 46570;
+
+    // Open dictionary file
+    FILE *dictfp = fopen(dict, "r");
+    // Check to see if it exists
+    if(dictfp == NULL)
+        err(1, "Empty dictionnary");
+
+    // Read in words from dictionary
+    while(fscanf(dictfp, "%s", word) != EOF){
+        printf("%s\n", word);
+        if(i < 46572)
+            word_list[i] = word;
+        i ++;
     }
-}
-
-char *correct(char *word)
-{
-    char **e1 = NULL;
-    char **e2 = NULL;
-    char *e1_word = NULL;
-    char *e2_word = NULL;
-    char *res_word = word;
-    int e1_rows = 0;
-    char e2_rows = 0;
-
-    if (find(word)) return word;
-
-    e1_rows = (unsigned)edits1_rows(word);
-    if (e1_rows)
-    {
-        e1 = edits1(word);
-        e1_word = max(e1, e1_rows);
-
-        if (e1_word)
-        {
-            array_cleanup(e1, e1_rows);
-            free(e1);
-            return e1_word;
-        }
-    }
-
-    e2 = known_edits2(e1, e1_rows, (int*)&e2_rows);
-    if (e2_rows)
-    {
-        e2_word = max(e2, e2_rows);
-        if (e2_word)
-            res_word = e2_word;
-    }
-
-    array_cleanup(e1, e1_rows);
-    array_cleanup(e2, e2_rows);
-
-    free(e1);
-    free(e2);
-
-    return res_word;
+    fclose(dictfp);
 }
--- src/cmd/spell2.h
+++ src/cmd/spell2.h
@@ -1,34 +1,19 @@
 #ifndef SPELLCHECK_H
 #define SPELLCHECK_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
-#include <search.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <sys/times.h>
+#include <err.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
-char *strtolower(char *word);
-ENTRY *find(char *word);
-int update(char *word);
-int readFile(const char* fileName, ENTRY dict);
-char *substr(char *str, size_t offset, size_t limit);
-static void *checked_malloc(int len);
-static void append(char *dst, int *dstLen, const char *src, int srcBegin, int len);
-int deletion(char *word, char **array, int start_idx);
-int transposition(char *word, char **array, int start_idx);
-int alteration(char *word, char **array, int start_idx);
-int insertion(char *word, char **array, int start_idx);
-size_t edits1_rows(char *word);
-char **edits1(char *word);
-int array_exist(char **array, int rows, char *word);
-char **known_edits2(char **array, int rows, int *e2_rows);
-char *max(char **array, int rows);
-void array_cleanup(char **array, int rows);
-char *correct(char *word);
+int levenshtein(const char *s, int ls, const char *t, int lt);
+char *correct(char *word, char **word_list);
+char** spell_check(char **text, int nb_word);
+void load(char *dict, char **word_list);
 
 #endif
