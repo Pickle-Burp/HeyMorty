71767a53d894e5f3a4668a6e19c5579acbb0c565
Annabelle Chevreau <annabelle.chevreau@gmail.com>
2021-06-16 14:24:36 +0200

    ui: delete \ qui casse tout

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -77,7 +77,7 @@ Ici vous trouverez toutes les actions que je peux realiser \n\n\n\
         * En utilisant votre navigateur par defaut : \n\
             Recherche [votre recherche] \n\
 * Trouver tout type de dossiers et fichiers presents sur l'ordinateur \n\
-    Demandez moi par exemple : \"trouve le dossier/fichier [dossier/fichier.extension]\"\n\</span>";
+    Demandez moi par exemple : \"trouve le dossier/fichier [dossier/fichier.extension]\"\n</span>";
 
     gtk_label_set_markup(GTK_LABEL(ui.pLabelHelp), ui.textHelp);
     gtk_label_set_justify(GTK_LABEL(ui.pLabel), GTK_JUSTIFY_CENTER);
