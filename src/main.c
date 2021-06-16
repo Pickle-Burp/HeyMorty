36d564f01a587cab9a0ebd61a7d44a769d4ee8aa
Vinetos <Vinetosdev@gmail.com>
2021-06-16 21:30:59 +0200

    feat: Add basic workflow !

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <json-c/json.h>
+#include <glib.h>
 #include "cmd/execution.h"
 #include "cmd/spell_check.h"
 #include "neural_network/nn.h"
@@ -11,7 +12,7 @@
 
 int main(int argc, char **argv) {
   if (argc < 2)
-    errx(EXIT_FAILURE, "No enough args. ./hey_morty <audio/nn/cmd/ui>");
+    errx(EXIT_FAILURE, "No enough args. ./hey_morty <audio/nn/cmd/ui/wk>");
   if (strcmp(argv[1], "audio") == 0) {
     if (argc < 3)
       errx(EXIT_FAILURE, "No enough args. ./hey_morty audio myFile.wav");
@@ -55,5 +56,35 @@ int main(int argc, char **argv) {
   }
   if (strcmp(argv[1], "ui") == 0) {
     ui_start(0, NULL);
+  }
+  if (strcmp(argv[1], "wk") == 0) {
+    if (argc < 3)
+      errx(EXIT_FAILURE, "No enough args. ./hey_morty wk myFile.wav");
+
+    char *json = sprec_recognize_wav(getenv(API_KEY), LANG,
+                                     argv[2], 16000);
+    // extract result
+    struct json_object *parsed_json = json_tokener_parse(json);
+    struct json_object *results = json_object_object_get(parsed_json,
+                                                         "results");
+    struct json_object *res = json_object_array_get_idx(results, 0);
+    struct json_object
+        *alternatives = json_object_object_get(res, "alternatives");
+    struct json_object *alt = json_object_array_get_idx(alternatives, 0);
+    struct json_object *conf = json_object_object_get(alt, "confidence");
+    struct json_object *trans = json_object_object_get(alt, "transcript");
+    double confidence = json_object_get_double(conf);
+    const char *str = json_object_get_string(trans);
+    printf("%f : %s", confidence, str);
+    json_object_put(parsed_json);
+    const char **text = (const char **) g_strsplit(str, " ", -1);
+    int n = 0;
+    while (text[n] != NULL) {
+      n++;
+    }
+    char **checked_text = spell_check((char **) text, n);
+    const char *command = convert_to_command(checked_text, n);
+    command_exec(command);
+    return 0;
   }
 }
